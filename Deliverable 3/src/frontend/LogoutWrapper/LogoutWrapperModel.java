eaae7c32335da9088e485cea759945c09c7d9b2b
basilicon <arcademedes@gmail.com>
2023-12-03 19:01:11 -0500

    Added LogoutWrapper components

---
+++ Deliverable 3/src/frontend/LogoutWrapper/LogoutWrapperModel.java
@@ -0,0 +1,5 @@
+package frontend.LogoutWrapper;
+
+public class LogoutWrapperModel {
+
+}
---
+++ Deliverable 3/src/frontend/LogoutWrapper/LogoutWrapperView.java
@@ -0,0 +1,15 @@
+package frontend.LogoutWrapper;
+
+import javax.swing.JComponent;
+
+import frontend.IWrapper;
+
+public class LogoutWrapperView extends JComponent implements IWrapper {
+
+	@Override
+	public void changeMVC(Object model, JComponent view, Object controller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
