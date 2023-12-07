b4213bd78481aeba16ea821916cc1647da87ea24
basilicon <arcademedes@gmail.com>
2023-12-07 13:56:13 -0500

    Update ProductSellerController.java
    Fixed issue where description field was not updating

--- Deliverable 3/src/frontend/ProductSeller/ProductSellerController.java
+++ Deliverable 3/src/frontend/ProductSeller/ProductSellerController.java
@@ -30,11 +30,11 @@ public class ProductSellerController {
 			@Override
 			public void keyTyped(KeyEvent e) {}
 			@Override
-			public void keyPressed(KeyEvent e) {
+			public void keyPressed(KeyEvent e) {}
+			@Override
+			public void keyReleased(KeyEvent e) {
 				onDescriptionFieldEdit();
 			}
-			@Override
-			public void keyReleased(KeyEvent e) {}
 		});
 		view.addPriceFieldListener(new KeyListener() {
 			@Override
