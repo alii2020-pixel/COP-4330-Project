71c8da80f5da677331758e45bebac6e14c74f428
basilicon <arcademedes@gmail.com>
2023-12-07 00:27:05 -0500

    Adjusted alignment for MainScreenCustomer

--- Deliverable 3/src/frontend/MainScreenCustomer/MainScreenCustomerModel.java
+++ Deliverable 3/src/frontend/MainScreenCustomer/MainScreenCustomerModel.java
@@ -21,7 +21,10 @@ public class MainScreenCustomerModel {
 		return new LinkedList<Product>(Arrays.asList(
 			new Product("Item 1", "An item you can buy", 100.00),
 			new Product("Item 2", "Another item you can buy", 200.00),
-			new Product("Item 3", "Last item you can buy", 300.00)
+			new Product("Item 3", "Last item you can buy", 300.00),
+			new Product("Item 4", "test", 35.125),
+			new Product("Item 5", "test2", 11.35),
+			new Product("Item 6", "test3", 78.46)
 		));
 	}
 }
